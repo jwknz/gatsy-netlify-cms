1262a27135d1e0fb147b6b325ce3ac8d6efd0eea
Jeff Kranenburg <jwkranenburg@icloud.com>
2024-04-18 06:55:08 +1200

    testing...100

--- src/pages/{markdownRemark.frontmatter__slug}.jsx
+++ src/pages/{markdownRemark.frontmatter__slug}.jsx
@@ -1,26 +1,73 @@
 import * as React from "react"
-import { graphql } from "gatsby"
+import { graphql, Link } from "gatsby"
+
+const pageStyles = {
+  padding: 40,
+}
 
 export default function BlogPostTemplate({
   data, // this prop will be injected by the GraphQL query below.
 }) {
+    
   const { markdownRemark } = data // data.markdownRemark holds your post data
   const { frontmatter, html } = markdownRemark
+
   return (
-    <div>
+    <main style={pageStyles}>
       <div>
-        <h1>{frontmatter.title}</h1>
-        <h2>{frontmatter.date}</h2>
-        <div
-          dangerouslySetInnerHTML={{ __html: html }}
-        />
+        <h1 className="font-bold text-2xl mb-2">Hello World!</h1>
+        <ul className="p-2 border-2 border-gray-400 mb-6">
+          {data.files.edges.map( ({node}) => {
+
+            console.log(node.frontmatter)
+
+            return (
+
+              <li key={node.id} className={[
+                  node.frontmatter.tags[0] === "guides" ? "bg-blue-400" : 
+                  node.frontmatter.tags[0] === "tests" ? "bg-red-400" :
+                  "bg-green-400",
+                  "m-2 p-2 rounded-lg"
+                  ].join(" ")}>
+                <Link to={`/${node.frontmatter.slug}`}>{node.frontmatter.title}</Link>
+              </li>
+
+            )
+          })}
+        </ul>
+
+        <div>
+          <h1>{frontmatter.title}</h1>
+          <h2>{frontmatter.date}</h2>
+          <div
+            dangerouslySetInnerHTML={{ __html: html }}
+          />
+        </div>
+
       </div>
-    </div>
+    </main>
   )
 }
 
+export const Head = () => <title>Home Page</title>
+
 export const pageQuery = graphql`
   query($id: String!) {
+
+    files: allMarkdownRemark {
+      edges {
+        node {
+          id
+          html
+          frontmatter {
+            title
+            tags
+            slug
+          }
+        }
+      }
+    }
+
     markdownRemark(id: { eq: $id }) {
       html
       frontmatter {
@@ -29,5 +76,6 @@ export const pageQuery = graphql`
         title
       }
     }
+
   }
 `
