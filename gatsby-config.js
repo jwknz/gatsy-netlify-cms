7eb603dac83910a6a8fd707afb28dfd4ee9dad49
Jeff Kranenburg <jwkranenburg@icloud.com>
2024-04-17 13:34:53 +1200

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `100-gatsby-netlify-cms`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-netlify-cms", "gatsby-plugin-postcss"]
+};
