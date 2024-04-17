c07e6fac5c29c9dc47f588521716f22710553ada
Jeff Kranenburg <jwkranenburg@icloud.com>
2024-04-17 20:39:19 +1200

    testing...8

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,19 +20,19 @@ module.exports = {
     {
       resolve : `gatsby-source-filesystem`,
       options: {
-        name: `md`,
-        path: `${__dirname}/src/content`
-      }
-    },
-    {
-      resolve : `gatsby-source-filesystem`,
-      options: {
         name: `images`,
         path: `${__dirname}/static`
       }
     },
     `gatsby-transformer-remark`,
     `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`
+    `gatsby-plugin-sharp`,
+    {
+      resolve : `gatsby-source-filesystem`,
+      options: {
+        name: `md`,
+        path: `${__dirname}/src/content`
+      }
+    }
   ]
 };
